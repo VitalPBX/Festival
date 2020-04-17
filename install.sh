1bff4e699bf16d501432b55a11bd9c22793d1111
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2020-04-17 07:23:40 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo -e "*     Welcome to the VitalPBX Festival TTS Integration     *"
 echo -e "************************************************************"
 yum install festival festival-freebsoft-utils -y
 cd /etc/systemd/system/
-wget https://github.com/VitalPBX/festival/raw/master/festival.service
+wget https://raw.githubusercontent.com/VitalPBX/Festival/master/festival.service
 chmod 664 /etc/systemd/system/festival.service
 systemctl daemon-reload
 systemctl start festival.service
@@ -26,11 +26,11 @@ yum install -y festvox-slt-arctic-hts
 yum install -y hispavoces-pal-diphone
 yum install -y hispavoces-sfl-diphone
 cd /etc/asterisk/
-wget https://github.com/VitalPBX/festival/raw/master/festival.conf
+wget https://raw.githubusercontent.com/VitalPBX/Festival/master/festival.conf
 asterisk -rx"module load app_festival.so"
 systemctl restart festival.service
 cd /etc/asterisk/ombutel
-wget https://github.com/VitalPBX/pico2wave/raw/master/extensions__90-festival.conf
+wget https://raw.githubusercontent.com/VitalPBX/Festival/master/extensions__90-festival.conf
 asterisk -rx"dialplan reload"
 echo -e "\n"
 echo -e "************************************************************"
