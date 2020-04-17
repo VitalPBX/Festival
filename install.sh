5392631e0edb6b782f0d5d76d0a238e1feb9525c
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2020-04-17 06:59:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,5 +25,7 @@ yum install -y festvox-rms-arctic-hts
 yum install -y festvox-slt-arctic-hts
 yum install -y hispavoces-pal-diphone
 yum install -y hispavoces-sfl-diphone
-
-
+cd /etc/asterisk/
+wget https://github.com/VitalPBX/festival/raw/master/festival.conf
+systemctl restart festival.service
+asterisk -rx"core reload"
